434a322881a197bbff2762e78f0d0632c67b5886
Marko <marko.bjorninen@gmail.com>
2014-11-24 20:35:54 +0200

    Velocity
    Added velocity

--- platforms/android/ant-build/classes.dex.d
+++ platforms/android/ant-build/classes.dex.d
@@ -1,11 +1,11 @@
-E:\DEV\LabyrintApp\platforms\android\ant-build\classes.dex : \
-E:\DEV\LabyrintApp\platforms\android\ant-build\classes\com\phonegap\helloworld\BuildConfig.class \
-E:\DEV\LabyrintApp\platforms\android\ant-build\classes\com\phonegap\helloworld\HelloWorld.class \
-E:\DEV\LabyrintApp\platforms\android\ant-build\classes\com\phonegap\helloworld\R$attr.class \
-E:\DEV\LabyrintApp\platforms\android\ant-build\classes\com\phonegap\helloworld\R$drawable.class \
-E:\DEV\LabyrintApp\platforms\android\ant-build\classes\com\phonegap\helloworld\R$string.class \
-E:\DEV\LabyrintApp\platforms\android\ant-build\classes\com\phonegap\helloworld\R$xml.class \
-E:\DEV\LabyrintApp\platforms\android\ant-build\classes\com\phonegap\helloworld\R.class \
-E:\DEV\LabyrintApp\platforms\android\ant-build\classes\org\apache\cordova\devicemotion\AccelListener$1.class \
-E:\DEV\LabyrintApp\platforms\android\ant-build\classes\org\apache\cordova\devicemotion\AccelListener.class \
-E:\DEV\LabyrintApp\platforms\android\ant-build\dexedLibs\classes-0c0c4eaf1115f5a02f1bfb78c1ad64f0.jar \
+Z:\Documents\GitHub\LabyrintApp\platforms\android\ant-build\classes.dex : \
+Z:\Documents\GitHub\LabyrintApp\platforms\android\ant-build\classes\com\phonegap\helloworld\BuildConfig.class \
+Z:\Documents\GitHub\LabyrintApp\platforms\android\ant-build\classes\com\phonegap\helloworld\HelloWorld.class \
+Z:\Documents\GitHub\LabyrintApp\platforms\android\ant-build\classes\com\phonegap\helloworld\R$attr.class \
+Z:\Documents\GitHub\LabyrintApp\platforms\android\ant-build\classes\com\phonegap\helloworld\R$drawable.class \
+Z:\Documents\GitHub\LabyrintApp\platforms\android\ant-build\classes\com\phonegap\helloworld\R$string.class \
+Z:\Documents\GitHub\LabyrintApp\platforms\android\ant-build\classes\com\phonegap\helloworld\R$xml.class \
+Z:\Documents\GitHub\LabyrintApp\platforms\android\ant-build\classes\com\phonegap\helloworld\R.class \
+Z:\Documents\GitHub\LabyrintApp\platforms\android\ant-build\classes\org\apache\cordova\devicemotion\AccelListener$1.class \
+Z:\Documents\GitHub\LabyrintApp\platforms\android\ant-build\classes\org\apache\cordova\devicemotion\AccelListener.class \
+Z:\Documents\GitHub\LabyrintApp\platforms\android\ant-build\dexedLibs\classes-62a1b62bd953b1eab82d8ae2aba6df32.jar \
