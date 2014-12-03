88f49597f85fbb12996371d172b11e053610feac
Marko <marko.bjorninen@gmail.com>
2014-12-03 20:00:47 +0200

    Maps + skins
    Added the maps and skins from the dropbox

--- platforms/android/ant-build/AndroidManifest.xml.d
+++ platforms/android/ant-build/AndroidManifest.xml.d
@@ -1,2 +1,2 @@
-E:\adt-bundle-windows-x86_64-20140702\Git\Git\LabyrintApp\platforms\android\ant-build\AndroidManifest.xml : \
-E:\adt-bundle-windows-x86_64-20140702\Git\Git\LabyrintApp\platforms\android\AndroidManifest.xml \
+Z:\Documents\GitHub\LabyrintApp\platforms\android\ant-build\AndroidManifest.xml : \
+Z:\Documents\GitHub\LabyrintApp\platforms\android\AndroidManifest.xml \
